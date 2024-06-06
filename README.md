b8aa1118b313f3dfe7a2adae6cd1273c6c6b4388
KojiIshikaw <koji.ishikawa@techshare.co.jp>
2024-06-06 22:17:30 +0800

    update

--- README.md
+++ README.md
@@ -8,10 +8,20 @@ docker build -t go2-humble:latest .
 ```
 
 
+```sh
+git clone https://github.com/TechShare-inc/go2_unitree_ros2.git -b imu_publisher
+git clone https://github.com/TechShare-inc/HesaiLidar_General_ROS-ROS2.git
+git clone https://github.com/unitreerobotics/unitree_ros2.git
+git clone https://github.com/TechShare-inc/direct_lidar_inertial_odometry.git
+```
 
- https://github.com/TechShare-inc/go2_unitree_ros2.git -b imu_publisher
- https://github.com/TechShare-inc/HesaiLidar_General_ROS-ROS2.git
- https://github.com/unitreerobotics/unitree_ros2.git
+```
+$ cd ..
+$ bash ../docker/humble.sh
+# cd external/
+# colcon build --symlink-install
+```
 
+unitree_ros2
+eth0
 
- https://github.com/vectr-ucla/direct_lidar_inertial_odometry.git -b feature/ros2
--- docker/humble.sh
+++ docker/humble.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-XAUTH=/tmp/.docker.xauth
+# xhost設定を追加してローカル接続を許可
+xhost +local:docker
 
-if [ ! -f $XAUTH ]
-then
+# XAUTHファイルの生成
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]; then
+    touch $XAUTH
     xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
-    if [ ! -z "$xauth_list" ]
-    then
+    if [ ! -z "$xauth_list" ]; then
         echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
     fi
     chmod a+r $XAUTH
 fi
 
+# Dockerコンテナの実行
 docker run -it --rm \
   --privileged \
   --net=host \
-  --env="DISPLAY" \
+  --env="DISPLAY=$DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
   --env="XAUTHORITY=$XAUTH" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --volume="$XAUTH:$XAUTH" \
   --volume="${PWD}:/external:rw" \
   go2-humble:latest bash
